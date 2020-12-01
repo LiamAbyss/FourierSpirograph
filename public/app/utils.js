f7351077280240e1d4665052f15730ac81f1907d
RemiArbache <32673658+RemiArbache@users.noreply.github.com>
2020-12-01 15:24:33 +0100

    Quick cosmetic fix

--- public/app/utils.js
+++ public/app/utils.js
@@ -4,8 +4,8 @@ let predefinedThresholdsFile = ''
 let computeSize = -1
 
 /**
- * Event listener on 'openbtn', toggles settings sidebar visibility.
- */
+* Event listener on 'openbtn', toggles settings sidebar visibility.
+*/
 // eslint-disable-next-line no-unused-vars
 const toggleNav = () => {
   const sidebar = document.getElementById('settingsSidebar')
@@ -19,16 +19,16 @@ const toggleNav = () => {
 }
 
 /**
-   * @typedef {Object} ThresholdPair
-   * @property {number} lt - The lower threshold
-   * @property {number} ut - The upper threshold
-   */
+* @typedef {Object} ThresholdPair
+* @property {number} lt - The lower threshold
+* @property {number} ut - The upper threshold
+*/
 
 /**
-   * Reads predefined threshold pairs from given .csv file and returns object list.
-   * @param {string} thresholdFile
-   * @returns {ThresholdPair[]}
-   */
+* Reads predefined threshold pairs from given .csv file and returns object list.
+* @param {string} thresholdFile
+* @returns {ThresholdPair[]}
+*/
 const getThresholdsFromCsv = (thresholdFile) => {
   const res = []
   const lines = thresholdFile.split('\r\n')
@@ -45,9 +45,9 @@ const getThresholdsFromCsv = (thresholdFile) => {
 }
 
 /**
-   * Requests the given file URL and sets the global variable predefinedThresholds
-   * @param {string} url - URL of the file containing the predefined thresholds
-   */
+* Requests the given file URL and sets the global variable predefinedThresholds
+* @param {string} url - URL of the file containing the predefined thresholds
+*/
 const setPredfinedThresholds = (url) => {
   // read text from URL location
   var request = new XMLHttpRequest()
@@ -64,10 +64,10 @@ const setPredfinedThresholds = (url) => {
 setPredfinedThresholds('../public/app/autoThresholds.csv')
 
 /**
-   * Applies blind mesh to given data : keeps one in (premeshSel.value) data points.
-   * @param {int[]} data - RGBA pixel array
-   * @returns {int[]} - Meshed RGBA pixel array
-   */
+* Applies blind mesh to given data : keeps one in (premeshSel.value) data points.
+* @param {int[]} data - RGBA pixel array
+* @returns {int[]} - Meshed RGBA pixel array
+*/
 const applyPremesh = (data) => {
   if (data === undefined || data.length === 0) return
   let cpt = 0
@@ -85,18 +85,18 @@ const applyPremesh = (data) => {
 }
 
 /**
-   * @typedef {Object} Point
-   * @property {number} x - The X coordinate
-   * @property {number} y - The Y coordinate
-   */
+* @typedef {Object} Point
+* @property {number} x - The X coordinate
+* @property {number} y - The Y coordinate
+*/
 
 /**
-   * Ignores pixels closer to the image border than the set margin.
-   * Reshapes data as point list.
-   * @param {int[]} data - RGBA pixel array
-   * @param {int} margin - Distance to border (px)
-   * @returns {Point[]} - Point list
-   */
+* Ignores pixels closer to the image border than the set margin.
+* Reshapes data as point list.
+* @param {int[]} data - RGBA pixel array
+* @param {int} margin - Distance to border (px)
+* @returns {Point[]} - Point list
+*/
 const applyMargin = (data, margin) => {
   const newData = []
 
@@ -115,10 +115,10 @@ const applyMargin = (data, margin) => {
 }
 
 /**
-   * Formats data as CSV to be sent to the spirogragp
-   * @param {Point[]} newData - Point list
-   * @returns {string} - Point CSV
-   */
+* Formats data as CSV to be sent to the spirogragp
+* @param {Point[]} newData - Point list
+* @returns {string} - Point CSV
+*/
 const makeCSV = (newData) => {
   if (newData.length === 0) return
   const rows = [['x', 'y']]
@@ -132,10 +132,10 @@ const makeCSV = (newData) => {
 }
 
 /**
-   * Decides what is to be done when the worker has finished.
-   * Launches spirograph if no other computations are running.
-   * @param {event} e - The message event sent by the worker
-   */
+* Decides what is to be done when the worker has finished.
+* Launches spirograph if no other computations are running.
+* @param {event} e - The message event sent by the worker
+*/
 const onWorkerMessage = (e) => {
   const drawBytesOnCanvasForImg = drawBytesOnCanvas(window.appData.width, window.appData.height)
   if (e.data.type === 'gradientMagnitude') {
@@ -165,16 +165,16 @@ const onWorkerMessage = (e) => {
 }
 
 /**
-   * Initialize worker.
-   */
+* Initialize worker.
+*/
 const initWorker = () => {
   worker = new Worker('../public/outline/worker.js')
   worker.addEventListener('message', onWorkerMessage, false)
 }
 
 /**
-   * Displays a preview image and clears predefinedComputedOutlines.
-   */
+* Displays a preview image and clears predefinedComputedOutlines.
+*/
 const previewImage = () => {
   var oFReader = new FileReader()
   oFReader.readAsDataURL(imageInput.files[0])
@@ -202,8 +202,8 @@ const previewImage = () => {
 }
 
 /**
-   * Computes all outlines with predefined thresholds. The results are stored in predefinedComputedOutlines.
-   */
+* Computes all outlines with predefined thresholds. The results are stored in predefinedComputedOutlines.
+*/
 const computeOutlines = () => {
   // Disables slider while computing
   slider.setAttribute('disabled', true)
@@ -234,9 +234,9 @@ const computeOutlines = () => {
 }
 
 /**
-   * Toggles between automatically computed outlines and manual threshold choices.
-   * Executes computeOutlines if not already done.
-   */
+* Toggles between automatically computed outlines and manual threshold choices.
+* Executes computeOutlines if not already done.
+*/
 const toggleThresholds = () => {
   // Do nothing if there is no image
   if (imgData === undefined) return
