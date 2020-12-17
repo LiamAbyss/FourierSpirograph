c3d9b57468451dfd6dccb92fb8cd8aa587d7b345
LiamAbyss <paul.bucamp@isen.yncrea.fr>
2020-12-17 21:33:00 +0100

    Hotfix examples

--- public/app/examples.js
+++ public/app/examples.js
@@ -4,7 +4,7 @@ fetch('examples.json').then(res => {
   const table = document.getElementById('table')
   let text = ''
   jsonFile.forEach(elt => {
-    text += `<tr onclick="importExample('example/${elt.name}');"><td>${elt.name}</td><td><img src='example/img/${elt.name}'/></td></tr>`
+    text += `<tr onclick="importExample('example/${elt.name.toLowerCase()}');"><td>${elt.name}</td><td><img src='example/img/${elt.name.toLowerCase()}'/></td></tr>`
   })
   table.innerHTML = text
 })
